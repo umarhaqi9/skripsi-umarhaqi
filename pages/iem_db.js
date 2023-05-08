db4cf64e3cc4e27d1ec55c5adda0924d074c0579
umarhaqi9 <74255063+umarhaqi9@users.noreply.github.com>
2023-05-08 20:03:50 +0700

    Create iem_db.js

---
+++ pages/iem_db.js
@@ -0,0 +1,13 @@
+export const IEM = [
+    {
+        id: "",
+        name: "",
+        price: "",
+        bass: 4,
+        mid: 3,
+        treble: 3,
+        linkBeli: "",
+        jumlahDriver: "",
+        tipeDriver: "",
+    }
+]
