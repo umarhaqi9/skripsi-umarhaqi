de7b864363f8c60dfcb40b88f8db2c22e90aede5
umarhaqi9 <74255063+umarhaqi9@users.noreply.github.com>
2023-05-22 12:31:40 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -370,7 +370,7 @@ export default function Home() {
               <div className='w-full px-4' data-aos="fade-up">
                 <div className='mx-auto text-center mb-16'>
                   <h4 className='font-semibold text-lg text-sky-500 mb-2'>IEMs</h4>
-                  <h2 className='font-bold text-black dark:text-white text-4xl mb-4  '>List of Products</h2>
+                  <h2 className='font-bold text-black dark:text-white text-4xl mb-4'>Daftar Produk</h2>
                   <p className='font-medium text-slate-500 dark:text-slate-300 md:text-lg'>
                   Berikut adalah beberapa IEM yang tersedia pada sistem ini:
                   </p>
